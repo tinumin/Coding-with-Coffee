264a6d4d7b196674ef249b4469d6a4cf35c90677
Tinu Mathai <48128271+tinumin@users.noreply.github.com>
2019-03-17 19:36:27 +0530

    Add files via upload

---
+++ java100Plus/StarPattern1.java
@@ -0,0 +1,26 @@
+// --------Star pattern--------
+
+/*
+        *  *  *  *  * 
+        *  *  *  *  * 
+        *  *  *  *  * 
+        *  *  *  *  * 
+        *  *  *  *  *       
+*/
+
+
+package CodingEveryDay;
+
+public class StarPattern1 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for(int j=5 ;j>0;j--) {
+			System.out.print(" * ");
+			}
+			System.out.println("");
+		
+		}
+	}
+
+}
