f34a9ed085d5ff5945165e8a83fa1112a668d2f5
Tinu Mathai <48128271+tinumin@users.noreply.github.com>
2019-03-24 08:47:03 +0530

    Add files via upload

---
+++ java100Plus/numberPattern4.java
@@ -0,0 +1,24 @@
+//--------- output --------
+/*
+ 5 
+ 5  4 
+ 5  4  3 
+ 5  4  3  2 
+ 5  4  3  2  1 
+*/
+
+package CodingEveryDay;
+
+public class numberPattern4  {
+
+	public static void main(String[] args) {
+		for(int i=5; i>=1; i--) {
+			for(int j=5; j>=i;j--) {
+				System.out.print(" "+j+ " ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
